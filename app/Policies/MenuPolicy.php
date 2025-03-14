a33b3685f9087eeb8dc1a78c0675d6461ec82c61
M1racle <crazyhl@163.com>
2025-03-14 10:45:55 +0800

    优化 菜单 policy

--- app/Policies/MenuPolicy.php
+++ app/Policies/MenuPolicy.php
@@ -2,6 +2,7 @@
 
 namespace App\Policies;
 
+use App\Models\Menu;
 use App\Models\User;
 
 class MenuPolicy
@@ -32,5 +33,26 @@ class MenuPolicy
     public function delete(User $user): bool
     {
         return $user->hasRole('super-admin') || $user->can('menu-delete');
+    }
+
+    /**
+     * 查看列表
+     * @param User $user
+     * @return bool
+     */
+    public function viewList(User $user): bool
+    {
+        return $user->hasRole('super-admin') || $user->can('menu-view-list');
+    }
+
+    /**
+     * 查看单项
+     * @param User $user
+     * @param Menu $menu
+     * @return bool
+     */
+    public function view(User $user, Menu $menu): bool
+    {
+        return $user->hasRole('super-admin') || $user->can('menu-view-list');
     }
 }
